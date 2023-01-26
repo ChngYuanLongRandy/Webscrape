d4e95139917ebbc9d5c9b3254f22b4eafd7d44e5
ChngYuanLongRandy <chngyuanlong@gmail.com>
2023-01-26 14:24:34 +0800

    [#14] Added Scraper Class

--- Career_Future/scrape.py
+++ Career_Future/scrape.py
@@ -1,12 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+from scraper import Scraper
 import hydra
+
+
 
 @hydra.main(config_path='./conf', config_name='config.yaml')
 def main(args):
-    pass
+
+    search_terms = args['settings']['search-terms']
+    scraper = Scraper(search_terms[0])
+    scraper._test_scrape()
+
+
+
 
 if __name__ == "__main__" :
     main()
